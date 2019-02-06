5538b408869931add3f2ddfc5348493485e20cca
Gösta <goesta@helta-skelta.de>
2019-02-06 22:05:22 +0100

    Added docker hub hooks

--- dockerStartServer.sh
+++ dockerStartServer.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-XMAGE_CONFIG=/xmage/mage-server/config/config.xml
+XMAGE_CONFIG=/xmage/config/config.xml
 
 sed -i -e "s#\(serverAddress=\)[\"].*[\"]#\1\"$XMAGE_DOCKER_SERVER_ADDRESS\"#g" ${XMAGE_CONFIG}
 sed -i -e "s#\(serverName=\)[\"].*[\"]#\1\"$XMAGE_DOCKER_SERVER_NAME\"#g" ${XMAGE_CONFIG}
